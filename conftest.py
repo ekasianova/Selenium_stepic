5e4235b8cb61d69da26dff324f35dc100ff39403
”ekasianova” <ekasianova@griddynamics.com>
2021-11-25 17:47:18 +0300

    Modified message in conftest

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
+                     help="Choose browser: chrome, also as a defoult")
     parser.addoption('--language', action='store', default=None,
                      help="Choose language in short form: for ex. ru, en")
 
